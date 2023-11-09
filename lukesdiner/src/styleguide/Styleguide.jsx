03740340c5e238c2c359abe088d6267f94603799
Maria Guglielmone <info.mariaguglielmone@gmail.com>
2023-11-09 09:31:53 +0100

    scss btn; created basic utilities

--- lukesdiner/src/styleguide/Styleguide.jsx
+++ lukesdiner/src/styleguide/Styleguide.jsx
@@ -1,7 +1,9 @@
+import Btn from '../components/buttons/Btn'
 import './Styleguide.scss'
 
 export default function Styleguide() {
     return <>
         <h1>Hola soy tu styleguide!</h1>
+        <Btn type='primary' text='Send'/>
     </>
 }
